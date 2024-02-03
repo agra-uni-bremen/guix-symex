7c706b5a7bc954c5b582da7fb09f3b96e182da20
Sören Tempel <soeren+git@soeren-tempel.net>
2024-02-03 10:17:05 +0100

    symex: symex-vp: Reformat with guix style

--- src/symex/packages/symex-vp.scm
+++ src/symex/packages/symex-vp.scm
@@ -14,30 +14,33 @@
     (package
       (name "symex-vp")
       (version (git-version "20230703" "0" commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                       (url "https://github.com/agra-uni-bremen/symex-vp")
-                       (commit commit)
-                       (recursive? #t)))
-                (file-name (git-file-name name version))
-                (sha256 (base32 "1mpcsaym9k0yjbgy50wah4g6i5mkgmb4yyzvgps8gnh3811h6wrj"))))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/agra-uni-bremen/symex-vp")
+               (commit commit)
+               (recursive? #t)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1mpcsaym9k0yjbgy50wah4g6i5mkgmb4yyzvgps8gnh3811h6wrj"))))
       (build-system cmake-build-system)
       (arguments
-        (list
-          #:tests? #f
-          #:phases
-          #~(modify-phases %standard-phases
-              (add-before 'configure 'change-directory
-                (lambda _ (chdir "vp")))
-              (replace 'install
-                (lambda* (#:key outputs #:allow-other-keys)
-                  (let* ((out (assoc-ref outputs "out"))
-                         (bin (string-append out "/bin")))
-                    (install-file "bin/symex-vp" bin)
-                    (install-file "bin/hifive-vp" bin)))))))
+       (list
+        #:tests? #f
+        #:phases #~(modify-phases %standard-phases
+                     (add-before 'configure 'change-directory
+                       (lambda _
+                         (chdir "vp")))
+                     (replace 'install
+                       (lambda* (#:key outputs #:allow-other-keys)
+                         (let* ((out (assoc-ref outputs "out"))
+                                (bin (string-append out "/bin")))
+                           (install-file "bin/symex-vp" bin)
+                           (install-file "bin/hifive-vp" bin)))))))
       (inputs (list llvm boost z3))
       (synopsis "Symbolic execution engine for RISC-V embedded firmware")
-      (description "Symbolically executes RISC-V firmware and supports SystemC peripherals")
+      (description
+       "Symbolically executes RISC-V firmware and supports SystemC peripherals")
       (home-page "https://agra.informatik.uni-bremen.de/projects/risc-v/")
       (license license:expat))))

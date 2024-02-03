62dfa9651dd4bea7d57a7e1949af6606d28f6619
Sören Tempel <soeren+git@soeren-tempel.net>
2024-02-03 05:25:02 +0100

    symex: angr: Refactor package
    * Add better synopsis and description text
    * Enable angr test suite

--- src/symex/packages/angr.scm
+++ src/symex/packages/angr.scm
@@ -7,6 +7,8 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system python)
   #:use-module (guix build-system pyproject)
+  #:use-module (gnu packages databases)
+  #:use-module (gnu packages check)
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages maths)
@@ -16,8 +18,38 @@
   #:use-module (gnu packages protobuf)
   #:use-module (gnu packages python-web)
   #:use-module (gnu packages version-control)
-  #:use-module (gnu packages engineering)
+  #:use-module ((gnu packages engineering) #:prefix engineering:)
   #:use-module (gnu packages python-xyz))
+
+;; XXX: angr needs unicorn 2.X, Guix only packages unicorn 1.X
+;;
+;; See https://issues.guix.gnu.org/63442
+(define-public unicorn
+  (package
+    (name "unicorn")
+    (version "2.0.1.post1")
+    (source (origin
+              (method url-fetch)
+              (uri (pypi-uri name version))
+              (sha256 (base32 "0mlfs8qfi0clyncfkbxp6in0cpl747510i6bqymwid43xcirbikz"))))
+    ;; NOTE: Unicorn 2 using pyproject to build
+    (build-system pyproject-build-system)
+    (native-inputs (list cmake pkg-config))
+    (home-page "http://www.unicorn-engine.org")
+    (synopsis "Unicorn CPU emulator framework")
+    (description "Uniforn is a lightweight, multi-platform, multi-architecture CPU emulator
+                 framework based on QEMU.")
+    (license license:gpl2+)))
+
+;; Custom python-capstone verison with a backport for a capstone regression.
+;;
+;; See: https://github.com/capstone-engine/capstone/pull/2240
+(define-public python-capstone
+  (package-with-patches
+    engineering:python-capstone
+    '("patches/capstone-fix-python-constants.patch")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-public python-rpyc
   (package
@@ -33,12 +65,13 @@
     (arguments (list #:tests? #f))
     (propagated-inputs (list python-hatchling python-plumbum))
     (home-page "https://github.com/tomerfiliba-org/rpyc")
-    (synopsis
-     "Remote Python Call (RPyC) is a transparent and symmetric distributed computing library")
-    (description
-     "Remote Python Call (RPyC) is a transparent and symmetric distributed computing
-library")
-    (license #f)))
+    (synopsis "Remote Python Call (RPyC) is a symmetric distributed computing library")
+    (description "This Python module enables remote procedure calls, clustering, and
+distributed-computing.  For this purpose, it makes use of object-proxying, a technique
+that employs python's dynamic nature, to overcome the physical boundaries between
+processes and computers, so that remote objects can be manipulated as if they
+were local.")
+    (license license:expat)))
 
 (define-public python-nampa
   (package
@@ -53,9 +86,11 @@ library")
     (build-system pyproject-build-system)
     (propagated-inputs (list python-future))
     (home-page "https://github.com/kenoph/nampa")
-    (synopsis "FLIRT signatures for python")
-    (description "FLIRT signatures for python")
-    (license #f)))
+    (synopsis "Python implementation of IDA Pro's FLIRT technology")
+    (description "This Python module implements the Fast Library Identification
+and Recognition Technology (FLIRT).  This technology is useful for identifying
+common library subroutines in disassembled binaries.")
+    (license license:lgpl3)))
 
 (define-public python-mulpyplexer
   (package
@@ -68,12 +103,10 @@ library")
                (base32
                 "0c5xzci1djy1yi9hxxh8g67l6ms8r7ad7ja20pv8hfbdysdrwkhl"))))
     (build-system pyproject-build-system)
-    (home-page "")
-    (synopsis
-     "A module that multiplexes interactions with lists of python objects.")
-    (description
-     "This package provides a module that multiplexes interactions with lists of
-python objects.")
+    (home-page "https://github.com/zardus/mulpyplexer/")
+    (synopsis "Multiplexes interactions with lists of Python objects")
+    (description "This module provides utilities for multiplexing
+interactions with lists of Python objects.")
     (license license:bsd-3)))
 
 (define-public python-cppheaderparser
@@ -89,10 +122,9 @@ python objects.")
     (build-system pyproject-build-system)
     (propagated-inputs (list python-ply))
     (home-page "http://senexcanis.com/open-source/cppheaderparser/")
-    (synopsis
-     "Parse C++ header files and generate a data structure representing the class")
+    (synopsis "Parser for C++ header files")
     (description
-     "Parse C++ header files and generate a data structure representing the class")
+     "Parse C++ header files and generate a data structure representing the class.")
     (license license:bsd-3)))
 
 (define-public python-pysmt
@@ -107,13 +139,14 @@ python objects.")
                 "0izcl2wma7sid4a8a2qc9rzi8fnffsc8p97cx6njb2sc9rnnly8c"))))
     (build-system pyproject-build-system)
     (arguments (list #:tests? #f))
-    (home-page "http://www.pysmt.org")
+    (home-page "https://github.com/pysmt/pysmt")
     (synopsis
-     "A solver-agnostic library for SMT Formulae manipulation and solving")
+     "A solver-agnostic library for SMT formula manipulation and solving")
     (description
-     "This package provides a solver-agnostic library for SMT Formulae manipulation
-and solving")
-    (license #f)))
+      "This Python module provides a solver-agnostic abstraction for
+working with Satisfiability Modulo Theory (SMT) formulas  For example,
+it allows manipulation and solving such formulas.")
+    (license license:asl2.0)))
 
 (define-public python-claripy
   (package
@@ -136,8 +169,11 @@ and solving")
     (arguments (list #:tests? #f))
     (propagated-inputs (list python-cachetools python-decorator python-pysmt z3))
     (home-page "https://github.com/angr/claripy")
-    (synopsis "An abstraction layer for constraint solvers")
-    (description "An abstraction layer for constraint solvers")
+    (synopsis "Abstraction layer for constraint solvers")
+    (description
+      "This Python module provides an abstraction layer for interacting
+with constraint solvers.  Specifically, it is intended to be used with
+SMT solvers and is built on top of the Z3 solver.")
     (license license:bsd-2)))
 
 (define-public python-pyvex
@@ -163,9 +199,12 @@ and solving")
     (propagated-inputs (list python-archinfo python-bitstring python-cffi))
     (inputs (list gcc-toolchain))
     (home-page "https://github.com/angr/pyvex")
-    (synopsis "A Python interface to libVEX and VEX IR")
+    (synopsis "Python interface to libVEX and VEX IR")
     (description
-     "This package provides a Python interface to libVEX and VEX IR")
+     "This package provides a Python interface the libVEX and VEX IR.
+VEX is the intermediate representation (also known as intermediate
+language) used by the Valgrind analysis tool.  As such, VEX is designed
+to enable all kinds of binary analysis tasks.")
     (license license:bsd-2)))
 
 (define-public python-cle
@@ -184,9 +223,33 @@ and solving")
                              python-sortedcontainers))
     (native-inputs (list python-cffi))
     (home-page "https://github.com/angr/cle")
-    (synopsis "|")
-    (description "|")
+    (synopsis "Python loader for binaries and their associated libraries")
+    (description
+      "CLE loads binaries and their associated libraries, resolves
+imports and provides an abstraction of process memory the same way as if
+it was loader by the operating system's loader.")
     (license license:bsd-2)))
+
+(define-public python-keystone-engine
+  (package
+    (name "python-keystone-engine")
+    (version "0.9.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "keystone-engine" version))
+       (sha256
+        (base32 "1xahdr6bh3dw5swrc2r8kqa8ljhqlb7k2kxv5mrw5rhcmcnzcyig"))))
+    (inputs (list cmake))
+    (build-system pyproject-build-system)
+    (home-page "https://www.keystone-engine.org")
+    (synopsis "Lightweight multi-platform, multi-architecture assembler framework")
+    (description
+      "Keystone is a lightweight multi-platform, multi-architecture
+assembler framework.  It supports a wide-range of different architectures
+and overs an intuitive architecture-neutral API for interacting with
+assembly for these architectures.")
+    (license license:gpl2)))
 
 (define-public python-archinfo
   (package
@@ -199,12 +262,16 @@ and solving")
                (base32
                 "037xfq3wcf8ngayxz9623l4646m780v2102mfbygpzbkkjha1966"))))
     (build-system pyproject-build-system)
+    (propagated-inputs
+      (list
+        python-capstone
+        python-keystone-engine))
     (arguments (list #:tests? #f))
     (home-page "https://github.com/angr/archinfo")
-    (synopsis
-     "Classes with architecture-specific information useful to other projects.")
+    (synopsis "Extract architecture-specific information from binaries")
     (description
-     "Classes with architecture-specific information useful to other projects.")
+     "Collection of classes that contain architecture-specific
+information.  Useful for cross-architecture tools (such as pyvex).")
     (license license:bsd-2)))
 
 (define-public python-ailment
@@ -220,8 +287,11 @@ and solving")
     (build-system pyproject-build-system)
     (arguments (list #:tests? #f))
     (home-page "https://github.com/angr/ailment")
-    (synopsis "The angr intermediate language.")
-    (description "The angr intermediate language.")
+    (synopsis "The angr intermediate language")
+    (description
+      "This Pythom module implements an Intermediate Language (IL) or
+Intermediate Representation (IR) used by the angr binary analysis
+platform.")
     (license license:bsd-2)))
 
 (define-public python-itanium_demangle
@@ -239,43 +309,53 @@ and solving")
     (arguments (list #:tests? #f))
     (home-page "https://github.com/whitequark/python-itanium_demangler")
     (synopsis "Pure Python Itanium C++ ABI demangler")
-    (description "Pure Python Itanium C++ ABI demangler")
+    (description
+      "This Python module provides an implementation of the Itanium C++
+ABI symbol mangling language.  The demangler generates an abstract
+syntax tree from mangled symbols, which can be used for directly
+extracting type information.")
     (license license:bsd-0)))
-
-(define-public unicorn
-  (package
-    (name "unicorn")
-    (version "2.0.1.post1")
-    (source (origin
-              (method url-fetch)
-              (uri (pypi-uri name version))
-              (sha256 (base32 "0mlfs8qfi0clyncfkbxp6in0cpl747510i6bqymwid43xcirbikz"))))
-    ;; NOTE: Unicorn 2 using pyproject to build
-    (build-system pyproject-build-system)
-    (native-inputs (list cmake pkg-config))
-    (home-page "http://www.unicorn-engine.org")
-    (synopsis "Unicorn CPU emulator framework")
-    (description "Uniforn is a lightweight, multi-platform, multi-architecture CPU emulator
-                 framework based on QEMU.")
-    (license license:gpl2+)))
 
 (define-public python-angr
   (package
     (name "python-angr")
     (version "9.2.46")
     (source (origin
-              (method url-fetch)
-              (uri (pypi-uri "angr" version))
+              (method git-fetch)
+              (patches '(;"patches/angr-addition-type-error.patch"
+                         "patches/angr-check-exec-deps.patch"))
+              (uri (git-reference
+                     (url "https://github.com/angr/angr")
+                     (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
               (sha256
                (base32
-                "0liv0afmp3fnmkscqpdq4gqgnhvfpj7jh2y5b2998drn30gyxcgb"))))
+                "18y9wyf7va7gvp9zd6lhw82j9a2x2ajsvbawh96xnxzml0jwlwjm"))))
     (build-system pyproject-build-system)
     (arguments
       (list
-        #:tests? #f
         #:phases
         #~(modify-phases %standard-phases
-            (add-before 'build 'set-cc-native
+            (add-after 'unpack 'patch-tests
+              (lambda* (#:key inputs #:allow-other-keys)
+                (let ((coreutils (assoc-ref inputs "coreutils")))
+                  (substitute* "tests/test_vault.py"
+                    (("/bin/false")
+                     (string-append coreutils "/bin/false")))
+                  (substitute* "tests/common.py"
+                    (("\\[\"cc\"\\]") "[\"gcc\"]")))))
+            (replace 'check
+              (lambda* (#:key inputs tests? #:allow-other-keys)
+                (when tests?
+                  (copy-recursively (assoc-ref inputs "binaries") "../binaries")
+                  (with-directory-excursion "tests"
+                    ;; test_mips32_missing_offset_in_instructions fails
+                    ;; with capstone 5 and passes with capstone 4. Might
+                    ;; be a capstone regressions, needs investigation.
+                    (invoke "pytest" "-vv" "-x" "--dist" "loadfile"
+                            "-k" "not test_mips32_missing_offset_in_instructions"
+                            "-n" (number->string (parallel-job-count)))))))
+            (add-before 'build 'set-cc
               (lambda _
                 (setenv "CC" "gcc"))))))
     (propagated-inputs (list python-ailment
@@ -300,33 +380,34 @@ and solving")
                              python-progressbar2
                              python-rpyc
                              python-sortedcontainers
+                             python-sqlalchemy
                              python-sympy
                              unicorn))
+    (native-inputs
+      `(("python-pytest" ,python-pytest)
+        ("python-pytest-xdist" ,python-pytest-xdist)
+        ("binaries"
+         ;; This repository ships several binaries used only for testing
+         ;; purpose.  The binaries are not executed and not part of the
+         ;; angr distribution, they are only used to test angr's binary
+         ;; analysis capabilities.  In the context of the GNU FSDG, these
+         ;; fails should be considered non-functional data.
+         ,(origin
+          (method git-fetch)
+          (uri (git-reference
+                 (url "https://github.com/angr/binaries")
+                 (commit (string-append "v" version))))
+          (file-name (git-file-name "angr-binaries" version))
+          (sha256
+            (base32
+              "1f286b2239zavxzwg1184hj1zs380cr9qr549mvy3vywvm8bsmgr"))))))
     (home-page "https://github.com/angr/angr")
     (synopsis
-     "A multi-architecture binary analysis toolkit, with the ability to perform dynamic symbolic execution and various static analyses on binaries")
+     "Multi-architecture binary analysis toolkit")
     (description
-     "This package provides a multi-architecture binary analysis toolkit, with the
-  ability to perform dynamic symbolic execution and various static analyses on
-  binaries")
+     "This package provides a versatile binary analysis platform with the
+  ability to perform dynamic symbolic execution as well as various
+  static analyses directly on binaries.  As such, it can be used for all
+  kinds of reverse engineering, vulnerability discovery, exploit
+  generation, and software testing purposes.")
     (license license:bsd-2)))
-
-(define-public python-angr-platforms
-  (let ((commit "06db4e6a594af47aaeb0a5071f2cdb9a8c30f7f5"))
-    (package
-      (name "python-angr-platforms")
-      (version (git-version "20230220" "0" commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                       (url "https://github.com/angr/angr-platforms")
-                       (commit commit)))
-                (file-name (git-file-name name version))
-                (sha256 (base32 "1pq5wm32zi6lm02k2mni6a5cw4nd2z87xqnchy3vzkkig46lggnc"))))
-      (build-system pyproject-build-system)
-      (arguments (list #:tests? #f))
-      (propagated-inputs (list python-angr python-cle python-archinfo python-pyvex))
-      (synopsis "A collection of extensions to angr")
-      (description "Support for handling various new platforms with angr")
-      (home-page "https://github.com/angr/angr-platforms")
-      (license license:expat))))
